c8312b023a77f74863fb70e92231b88cab21bbf4
Jhan Bayer <jhanbayer@gmail.com>
2022-10-31 22:28:33 -0300

    feat: Add models

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { hot } from "react-hot-loader";
+import TodoList from "./todos/TodoList";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <p>Hello World v4</p>
+    <div className="App">
+      <TodoList />
     </div>
   );
 };
