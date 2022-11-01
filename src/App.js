3f79d129ec63161422d01a352c95f08c44fe0cae
Jhan Bayer <jhanbayer@gmail.com>
2022-10-31 21:34:51 -0300

    first commit React handmade

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello World v4</p>
+    </div>
+  );
+};
+
+export default hot(module)(App);
