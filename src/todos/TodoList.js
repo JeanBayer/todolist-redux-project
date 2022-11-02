5513df26be00ef764f7e9bd282007ecd78b7d517
Jhan Bayer <jhanbayer@gmail.com>
2022-11-02 18:54:42 -0300

    feat: Add reselect

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -4,10 +4,16 @@ import { completeTodoRequest, loadTodos, removeTodoRequest } from "./thunks";
 import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.css";
-import { getTodos, getTodosLoading } from "./selectors";
+import {
+  getCompletedTodos,
+  getIncompleteTodos,
+  getTodos,
+  getTodosLoading,
+} from "./selectors";
 
 const TodoList = ({
-  todos = [],
+  completedTodos,
+  incompleteTodos,
   onRemovePressed,
   onCompletedPressed,
   isLoading,
@@ -22,27 +28,24 @@ const TodoList = ({
   const content = (
     <div className="list-wrapper">
       <NewTodoForm />
-      {todos
-        .filter((todo) => !todo.isCompleted)
-        .map((todo) => (
-          <TodoListItem
-            key={todo.id}
-            todo={todo}
-            onRemovePressed={onRemovePressed}
-            onCompletedPressed={onCompletedPressed}
-          />
-        ))}
-      <hr />
-      {todos
-        .filter((todo) => todo.isCompleted)
-        .map((todo) => (
-          <TodoListItem
-            key={todo.id}
-            todo={todo}
-            onRemovePressed={onRemovePressed}
-            onCompletedPressed={onCompletedPressed}
-          />
-        ))}
+      <h3>Incomplete:</h3>
+      {incompleteTodos.map((todo) => (
+        <TodoListItem
+          key={todo.id}
+          todo={todo}
+          onRemovePressed={onRemovePressed}
+          onCompletedPressed={onCompletedPressed}
+        />
+      ))}
+      <h3>Completed:</h3>
+      {completedTodos.map((todo) => (
+        <TodoListItem
+          key={todo.id}
+          todo={todo}
+          onRemovePressed={onRemovePressed}
+          onCompletedPressed={onCompletedPressed}
+        />
+      ))}
     </div>
   );
 
@@ -50,7 +53,8 @@ const TodoList = ({
 };
 
 const mapStateToProps = (state) => ({
-  todos: getTodos(state),
+  completedTodos: getCompletedTodos(state),
+  incompleteTodos: getIncompleteTodos(state),
   isLoading: getTodosLoading(state),
 });
 
